e7cf3f81da22f866a3e33fb9714cadd4c9e3cbf6
Connor Watson <connorjosephwatson@hotmail.com>
2025-03-07 09:18:14 +0000

    fixed issue with  header & footer naming

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import Header from './components/Reuseable/Header.vue'
+import Header from './components/Reuseable/reuseHeader.vue'
 import { RouterView } from 'vue-router'
-import Footer from './components/Reuseable/Footer.vue'
+import Footer from './components/Reuseable/reuseFooter.vue'
 import { provideAccountStatus } from './composables/useAccountStatus'
 
 provideAccountStatus()
