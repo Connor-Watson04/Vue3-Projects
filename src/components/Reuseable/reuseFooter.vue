07ad1798e42f24704a1f21d785c5e4684e54b5ef
Connor Watson <connorjosephwatson@hotmail.com>
2025-03-21 15:37:38 +0000

    Updated footer css to tailwind

--- src/components/Reuseable/reuseFooter.vue
+++ src/components/Reuseable/reuseFooter.vue
@@ -1,123 +1,56 @@
 <template>
-  <footer class="footer">
-    <span class="footer-content">
-      <div class="Footer-img">
-        <img class="footer-logo" src="/src/assets/Images/Logo/S-I-S Logo.png" alt="footer logo" />
+  <footer class="mt-[10px] bg-[var(--color-promo)] py-[2rem] px-[1rem] w-[100vw]">
+    <span>
+      <div class="flex items-center justify-between gap-[25rem]">
+        <img class="h-[100px]" src="/src/assets/Images/Logo/S-I-S Logo.png" alt="footer logo" />
+        <span class="flex flex-row w-full gap-[12rem]">
+          <h1 class="w-1/2 text-center text-lg underline">Informational Pages</h1>
+          <h1 class="w-1/2 text-lg text-center underline">Our Links</h1>
+        </span>
       </div>
-      <div class="footer-grid">
-        <div class="footer-grid-left">
-          <p class="apologies">
+      <div class="flex gap-[5rem] select-none">
+        <div>
+          <p class="text-[#ebebeb] w-[40vw]">
             Apologies as we currently do not have a physical location for you to reach out to us.
             However we have our social media and content platforms where we may be reachable.
           </p>
         </div>
-        <div class="Details">
-          <a class="Detail-links" href="#">About Us</a>
-          <a class="Detail-links" href="#">FAQs</a>
-          <a class="Detail-links" href="#">Contact Us</a>
-          <a class="Detail-links" href="#">Terms & Service</a>
-          <a class="Detail-links" href="#">Privacy Policy</a>
+        <div class="w-full">
+       
+          <div class="grid grid-cols-2 text-[#4f8cff] w-full">
+            <a class="hover:-translate-y-[2px] hover:text-base active:text-[#3434dd] active:translte-y-[2px] h-[30px]" href="#">About Us</a>
+            <a class="hover:-translate-y-[2px] hover:text-base active:text-[#3434dd] active:translte-y-[2px] h-[30px]" href="#">FAQs</a>
+            <a class="hover:-translate-y-[2px] hover:text-base active:text-[#3434dd] active:translte-y-[2px] h-[30px]" href="#">Contact Us</a>
+            <a class="hover:-translate-y-[2px] hover:text-base active:text-[#3434dd] active:translte-y-[2px] h-[30px]" href="#">Terms & Service</a>
+            <a class="hover:-translate-y-[2px] hover:text-base active:text-[#3434dd] active:translte-y-[2px] h-[30px]" href="#">Privacy Policy</a>
+          </div>
         </div>
-        <div class="Our-links">
-          <a class="social-links" href="#">N1ghtW0lf04</a>
-          <a class="social-links" href="https://beacons.ai/ferralwolf2003">FerralWolf2003</a>
-          <a class="social-links" href="#">ibedrippinYT</a>
+       
+          <div class="flex flex-col gap-[1rem] w-1/3 text-[#4f8cff]">
+            <a class="hover:-translate-y-[2px] hover:text-base active:text-[#3434dd] active:translte-y-[2px] h-[20px]" href="#">N1ghtW0lf04</a>
+            <a class="hover:-translate-y-[2px] hover:text-base active:text-[#3434dd] active:translte-y-[2px] h-[20px]" href="https://beacons.ai/ferralwolf2003">FerralWolf2003</a>
+            <a class="hover:-translate-y-[2px] hover:text-base active:text-[#3434dd] active:translte-y-[2px] h-[20px]" href="#">ibedrippinYT</a>
+          </div>
         </div>
-      </div>
-      <div class="footer-social-icons">
-        <p class="copyright">&copy; 2026 S-I-S SQUAD</p>
+   
+      <div class="text-white flex gap-[10px]">
+        <p class="select-none">&copy; 2026 S-I-S SQUAD</p>
+        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+          <YoutubeIcon class="w-[24px] cursor-pointer"/>
+        </a>
+        <a href="https://twitch.tv" target="_blank" rel="noopener noreferrer">
+          <TwitchIcon class="w-[24px]"/>
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <InstagramIcon class="w-[24px]"/>
+        </a>
       </div>
     </span>
   </footer>
 </template>
-<script setup></script>
+<script setup>
+import InstagramIcon from './icons/instagramIcon.vue';
+import TwitchIcon from './icons/twitchIcon.vue';
+import YoutubeIcon from './icons/youtubeIcon.vue';
 
-<style>
-.footer {
-  background-color: var(--color-promo);
-  padding: 1rem 2rem;
-  width: 100vw;
-}
-.footer-logo {
-  height: 100px;
-  margin: 0;
-}
-.apologies {
-  color: #ebebeb;
-  width: 40vw;
-}
-.Our-links {
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 1rem;
-}
-.footer-grid {
-  display: grid;
-  grid-template-columns: 450px 400px 300px;
-  gap: 7rem;
-  user-select: none;
-}
-.Details {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-}
-.social-links,
-.Detail-links {
-  cursor: pointer;
-  color: #4f8cff;
-  height: 20px;
-  width: 100%;
-  text-decoration: none;
-}
-
-.social-links:hover,
-.Detail-links:hover {
-  color: #9595ff;
-  transform: translateY(-2px);
-  font-size: 17px;
-}
-
-.social-links:active,
-.Detail-links:active {
-  color: #3434dd;
-  transform: translateY(2px);
-  font-size: 14px;
-}
-
-.footer-social-icons {
-  display: flex;
-  gap: 0.8rem;
-  align-items: center;
-  color: #fff;
-}
-
-.copyright {
-  user-select: none;
-  font-size: 14px;
-}
-
-.footer-icon {
-  color: #d8d8d8;
-  cursor: pointer;
-}
-.yt:hover,
-.yt:active {
-  color: #ff0000;
-}
-.twitch:hover,
-.twitch:active {
-  color: #6705d8;
-}
-.fb:hover,
-.fb:active {
-  color: #356ee9;
-}
-.twitter:hover,
-.twitter:active {
-  color: #00e1ff;
-}
-.insta:hover,
-.insta:active {
-  color: #cd06ff;
-}
-</style>
+</script>
